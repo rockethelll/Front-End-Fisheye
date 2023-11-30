efb5d3dc48c7023118fa42b83cda1a588086de43
rockethelll <51137055+rockethelll@users.noreply.github.com>
2023-11-30 18:32:44 +0100

    can navigate with arrows key and escape on the lightbox

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,7 +1,7 @@
 export const displayLightbox = (medias) => {
   const lightbox = document.querySelector('.lightbox');
   const nextBtn = document.querySelector('.lightbox__next');
-  const previousBtn = document.querySelector('.lightbox__previous');
+  const previousBtn = document.querySelector('.lightbox__prev');
   const closeBtn = document.querySelector('.lightbox__close');
   const lightboxMedia = document.querySelector('.lightbox__media');
   const lightboxTitle = document.querySelector('.lightbox__title');
@@ -32,10 +32,55 @@ export const displayLightbox = (medias) => {
 
     lightboxMedia.innerHTML = `
       ${currentMedia.image ? `
-      <img src="./assets/images/${photographer.name}/${currentMedia.image}" alt="${currentMedia.title}">` 
-      : 
-      `<video controls aria-label="${currentMedia.title}"><source src="./assets/images/${photographer.name}/${currentMedia.video}" type="video/mp4"></video>`}
+      <img src="./assets/images/${photographer.name}/${currentMedia.image}" alt="${currentMedia.title}">`
+      :
+      `<video controls autoplay loop muted aria-label="${currentMedia.title}"><source src="./assets/images/${photographer.name}/${currentMedia.video}" type="video/mp4"></video>`}
     `;
     lightboxTitle.innerHTML = currentMedia.title;
   }
+
+  const closeLightbox = () => {
+    lightbox.style.display = 'none';
+    lightbox.setAttribute('aria-hidden', 'true')
+
+    const main = document.querySelector('main');
+    main.setAttribute('aria-hidden', 'false');
+    lightboxMedia.innerHTML = '';
+  }
+
+  const nextMedia = () => {
+    currentIndex++;
+    if (currentIndex > mediasList.length - 1) {
+      currentIndex = 0;
+    }
+    lightboxContent();
+  }
+
+  const previousMedia = () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = mediasList.length - 1;
+    }
+    lightboxContent();
+  }
+
+  const handleKeydown = e => {
+    switch (e.key) {
+      case 'Escape':
+        closeLightbox();
+        break;
+      case 'ArrowRight':
+        nextMedia();
+        break;
+      case 'ArrowLeft':
+        previousMedia();
+        break;
+    }
+  }
+
+  document.addEventListener('keydown', handleKeydown);
+
+  closeBtn.addEventListener('click', () => closeLightbox());
+  nextBtn.addEventListener('click', () => nextMedia());
+  previousBtn.addEventListener('click', () => previousMedia());
 }
