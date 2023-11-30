ef7ea3506b26f1b6ab013346265d877689ed40e2
rockethelll <51137055+rockethelll@users.noreply.github.com>
2023-11-30 15:46:32 +0100

    --amend

--- scripts/utils/likes.js
+++ scripts/utils/likes.js
@@ -12,22 +12,28 @@ export const toggleHeartIcon = () => {
   const heartsUnliked = document.querySelectorAll('.unliked');
   const heartsLiked = document.querySelectorAll('.liked');
 
+  // Toggle heart icon when like-unlike a media
   heartsUnliked.forEach(heartUnliked => {
     heartUnliked.addEventListener('click', () => {
       const heartLiked = heartUnliked.nextElementSibling;
+      // Remove the class 'visible' to the heart unliked and add it to the heart liked
       heartUnliked.classList.toggle('visible');
       heartLiked.classList.toggle('visible');
+      // Check if the heart liked has the class 'visible' and change the aria-hidden attribute
       heartLiked.classList.contains('visible');
       heartLiked.setAttribute('aria-hidden', 'false');
       heartUnliked.setAttribute('aria-hidden', 'true');
     })
   });
 
+  // Toggle heart icon when like-unlike a media
   heartsLiked.forEach(heartLiked => {
     heartLiked.addEventListener('click', () => {
       const heartUnliked = heartLiked.previousElementSibling;
+      // Remove the class 'visible' to the heart liked and add it to the heart unliked
       heartLiked.classList.toggle('visible');
       heartUnliked.classList.toggle('visible');
+      // Check if the heart unliked has the class 'visible' and change the aria-hidden attribute
       heartUnliked.classList.contains('visible');
       heartLiked.setAttribute('aria-hidden', 'true');
       heartUnliked.setAttribute('aria-hidden', 'false');
